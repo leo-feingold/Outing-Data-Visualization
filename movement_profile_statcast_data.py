18bcce0bfbbb56c8ff3bd3795a22f66111dc4a84
leo-feingold <113307524+leo-feingold@users.noreply.github.com>
2024-05-11 22:41:31 -0400

    added pitch result to plot

--- movement_profile_statcast_data.py
+++ movement_profile_statcast_data.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import datetime
 from baseball_scraper import statcast
+from baseball_scraper import playerid_lookup # Might be worthwhile to use ID's instead of names to filter by pitcher...
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
@@ -19,7 +20,7 @@ def scrape_data():
 
 def clean_data(df):
     df = scrape_data()
-    df = df[['pitcher', 'player_name', 'pitch_type', 'release_speed', 'pfx_x', 'pfx_z', 'release_spin_rate', 'spin_axis', 'release_extension', 'pitch_name']]
+    df = df[['pitcher', 'player_name', 'pitch_type', 'release_speed', 'pfx_x', 'pfx_z', 'release_spin_rate', 'spin_axis', 'release_extension', 'pitch_name', 'description', 'events', 'bb_type', 'type', 'estimated_woba_using_speedangle']]
     df['pfx_x'] = df['pfx_x'] * -12
     df['pfx_z'] = df['pfx_z'] * 12
     return df
@@ -33,7 +34,7 @@ def plot_data(df):
 
     df = clean_data(scrape_data())
     pitcher_data = df[df['player_name'] == playerName]
-    pitcher_data = pitcher_data.dropna(subset=['pfx_x', 'pfx_z', 'release_speed', 'release_spin_rate', 'spin_axis', 'release_extension', 'pitch_type'])
+    pitcher_data = pitcher_data.dropna(subset=['pfx_x', 'pfx_z', 'release_speed', 'release_spin_rate', 'spin_axis', 'release_extension', 'pitch_type', 'description', 'estimated_woba_using_speedangle'])
 
     plt.figure(figsize=(10, 6))
     sns.scatterplot(x='pfx_x', y='pfx_z', data=pitcher_data, hue='pitch_type', style='pitch_type', s=100, palette='deep')
@@ -50,17 +51,18 @@ def plot_data_interactive(df):
     prev_day = today - datetime.timedelta(days=1)
     prev_day = prev_day.strftime('%Y-%m-%d')
 
-    df = clean_data(df)  # Assuming df is already cleaned and passed here
+    df = clean_data(df)
     pitcher_data = df[df['player_name'] == playerName]
     pitcher_data = pitcher_data.dropna(subset=['pfx_x', 'pfx_z', 'release_speed', 'release_spin_rate', 'spin_axis'])
+    condition = pitcher_data['description'] == 'hit_into_play'
+    pitcher_data.loc[condition, 'description'] += " || xwOBA: " + pitcher_data.loc[:,'estimated_woba_using_speedangle'].astype(str)
 
-    # Create an interactive scatter plot
-    fig = px.scatter(pitcher_data, x='pfx_x', y='pfx_z', color='pitch_type',
-                     hover_data=['release_speed', 'release_spin_rate', 'spin_axis', 'release_extension'],
+    fig = px.scatter(pitcher_data, x='pfx_x', y='pfx_z', color='pitch_name',
+                     hover_data=['release_speed', 'release_spin_rate', 'spin_axis', 'release_extension', 'description'],
                      labels={
                          'pfx_x': 'Horizontal Movement (inches)',
                          'pfx_z': 'Vertical Movement (inches)',
-                         'pitch_type': 'Pitch Type'
+                         'pitch_name': 'Pitch Type'
                      },
                      title=f"Pitch Movement Profile for {playerName}, Date: {prev_day} (Pitcher's Perspective)")
     fig.show()
