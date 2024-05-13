34c02b20048ce07c03284f3746aecbe1850a35b2
leo-feingold <113307524+leo-feingold@users.noreply.github.com>
2024-05-13 08:31:06 -0400

    commit

--- movement_profile_statcast_data.py
+++ movement_profile_statcast_data.py
@@ -7,13 +7,16 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 
+
+# fix time delta, currently set for 2 days ago!
 playerName = 'Ragans, Cole'
+pitcherPerspectiveMode = False
 
 def scrape_data():
     today = datetime.date.today()
     today_str = today.strftime('%Y-%m-%d')
 
-    prev_day = today - datetime.timedelta(days=1)
+    prev_day = today - datetime.timedelta(days=2)
     prev_day = prev_day.strftime('%Y-%m-%d')
     data = statcast(start_dt = prev_day, end_dt = today_str)
     return data
@@ -21,16 +24,28 @@ def scrape_data():
 def clean_data(df):
     df = scrape_data()
     df = df[['pitcher', 'player_name', 'pitch_type', 'release_speed', 'pfx_x', 'pfx_z', 'release_spin_rate', 'spin_axis', 'release_extension', 'pitch_name', 'description', 'events', 'bb_type', 'type', 'estimated_woba_using_speedangle', 'p_throws']]
-    #df['pfx_x'] = df['pfx_x'] * -12
-    df['pfx_x'] = np.where(df['p_throws'] == 'R', df['pfx_x'] * -12, df['pfx_x'] * 12)
+    
+    # Pitcher Perspective Visualization Mode
+    if pitcherPerspectiveMode:
+        df['pfx_x'] = df['pfx_x'] * -12
+    # + is Arm Side, - is Glove Side, for data processing mode
+    else:
+        df['pfx_x'] = np.where(df['p_throws'] == 'R', df['pfx_x'] * -12, df['pfx_x'] * 12)
+    
     df['pfx_z'] = df['pfx_z'] * 12
     return df
 
 def plot_data(df):
+    myStr = ""
+    if pitcherPerspectiveMode:
+        myStr = "Pitcher's Perspective"
+    else:
+        myStr = "Arm Side +, Glove Side -"
+
     today = datetime.date.today()
     today_str = today.strftime('%Y-%m-%d')
 
-    prev_day = today - datetime.timedelta(days=1)
+    prev_day = today - datetime.timedelta(days=2)
     prev_day = prev_day.strftime('%Y-%m-%d')
 
     df = clean_data(scrape_data())
@@ -39,7 +54,7 @@ def plot_data(df):
 
     plt.figure(figsize=(10, 6))
     sns.scatterplot(x='pfx_x', y='pfx_z', data=pitcher_data, hue='pitch_type', style='pitch_type', s=100, palette='deep')
-    plt.title(f"Pitch Movement Profile for {playerName}, Date: {prev_day} (Pitcher's Perspective)")
+    plt.title(f"Pitch Movement Profile for {playerName}, Date: {prev_day} ({myStr})")
     plt.xlabel('Horizontal Movement (pfx_x)')
     plt.ylabel('Vertical Movement (pfx_z)')
     plt.legend(title='Pitch Type')
@@ -47,9 +62,14 @@ def plot_data(df):
     plt.show()
 
 def plot_data_interactive(df):
+    myStr = ""
+    if pitcherPerspectiveMode:
+        myStr = "Pitcher's Perspective"
+    else:
+        myStr = "Arm Side +, Glove Side -"
     today = datetime.date.today()
     today_str = today.strftime('%Y-%m-%d')
-    prev_day = today - datetime.timedelta(days=1)
+    prev_day = today - datetime.timedelta(days=2)
     prev_day = prev_day.strftime('%Y-%m-%d')
 
     df = clean_data(df)
@@ -65,7 +85,7 @@ def plot_data_interactive(df):
                          'pfx_z': 'Vertical Movement (inches)',
                          'pitch_name': 'Pitch Type'
                      },
-                     title=f"Pitch Movement Profile for {playerName} ({pitcher_data.p_throws.iloc[0]}HP), Date: {prev_day} (Arm Side+, Glove Side-)")
+                     title=f"Pitch Movement Profile for {playerName} ({pitcher_data.p_throws.iloc[0]}HP), Date: {prev_day} ({myStr})")
     fig.show()
 
 
